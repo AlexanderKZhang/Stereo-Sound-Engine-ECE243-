6ed7c7928da20dd971eba3c916742f3318273c5c
adropz <shaojiazhu71@gmail.com>
2026-03-20 11:50:54 -0400

    Update Audio c

--- audio.c
+++ audio.c
@@ -1,10 +1,9 @@
-#include "audio_array.h"
+#include "Antila_Floriography.h"
 
 #define AUDIO_BASE 0xFF203040
 #define SCALE_FACTOR 1.5
 
-// If array has 687,168 bytes, it has 171,792 32-bit words
-#define AUDIO_WORD_COUNT (687168 / 4)
+#define AUDIO_WORD_COUNT (1852068 / 2)
 
 int main(void) {
   struct audio_t {
@@ -16,14 +15,13 @@ int main(void) {
 
   struct audio_t* const audiop = ((struct audio_t*)AUDIO_BASE);
 
-  int word_index_counter = 0;
+  int left_index_counter = 30;
+  int right_index_counter = 0;
   int left, right;
 
   // THE MAGIC TRICK:
-  // Cast your 8-bit byte array into a 32-bit integer array.
-  // (Replace 'bombinsound...' with your exact array name)
-  const int* audio_word_array =
-      (const int*)bombinsound_the_motherx27s_day_21_second;
+  // Cast the 8-bit byte array into a 16-bit integer array.
+  const short* audio_word_array = (const short*)Antila_Floriography;
 
   while (1) {
     unsigned int space = audiop->fifospace;
@@ -32,23 +30,28 @@ int main(void) {
     int wslc = (space & 0xFF000000) >> 24;
 
     if ((wsrc > 0) && (wslc > 0)) {
-      // Read the fully assembled 32-bit word directly from the casted array!
-      int raw_sample_word = audio_word_array[word_index_counter];
+      // Cast
+      int left_raw_sample_word = audio_word_array[left_index_counter] << 16;
+      int right_raw_sample_word = audio_word_array[right_index_counter] << 16;
 
-      // Apply the volume scale factor
-      left = (int)(raw_sample_word * SCALE_FACTOR);
-      right = left;
+      // Apply the volume scale factor if needed later
+      left = (int)(left_raw_sample_word);
+      right = right_raw_sample_word;
 
       // Write to the hardware FIFOs
       audiop->left_fifo = left;
       audiop->right_fifo = right;
 
       // Advance the index. (Change to += 11 if you still need to speed it up)
-      word_index_counter++;
+      left_index_counter++;
+      right_index_counter++;
 
       // Wrap around using the WORD count, not the BYTE size
-      if (word_index_counter >= AUDIO_WORD_COUNT) {
-        word_index_counter = 0;
+      if (left_raw_sample_word >= AUDIO_WORD_COUNT) {
+        left_raw_sample_word = 0;
+      }
+      if (right_raw_sample_word >= AUDIO_WORD_COUNT) {
+        right_raw_sample_word = 0
       }
     }
   }
