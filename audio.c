11fd7d9282fdcb7aca31e922215edab91bf8f543
adropz <shaojiazhu71@gmail.com>
2026-03-25 23:06:40 -0400

    Added a convolution with the HRTF database

--- audio.c
+++ audio.c
@@ -1,4 +1,5 @@
 #include "Antila_Floriography.h"
+#include "hrtf_matrix.h"
 
 #define AUDIO_BASE 0xFF203040
 #define SCALE_FACTOR 1.5
@@ -13,9 +14,12 @@ struct audio_t {
 
 struct audio_t* const audiop = ((struct audio_t*)AUDIO_BASE);
 
-int left_index_counter = 30;
+int left_index_counter = 0;
 int right_index_counter = 0;
 int left, right;
+
+//function prototype
+int* convolve(const short* audio_word_array, int* result);
 
 void audio_setup(void) {
   // setting 1 to the WE bit to generate an interrupt when either of the Write FIFOs are less that 25% full
@@ -35,9 +39,11 @@ void handle_audio(void) {
 
     if ((wsrc > 0) && (wslc > 0)) {
       // Cast
-      int left_raw_sample_word = audio_word_array[left_index_counter] << 16;
-      int right_raw_sample_word = audio_word_array[right_index_counter] << 16;
-
+      //int left_raw_sample_word = audio_word_array[left_index_counter] << 16;
+      //int right_raw_sample_word = audio_word_array[right_index_counter] << 16;
+      int left_raw_sample_word = audio_word_array[left_index_counter];
+      int right_raw_sample_word = audio_word_array[right_index_counter];
+      
       // Apply the volume scale factor if needed later
       left = (int)(left_raw_sample_word);
       right = right_raw_sample_word;
@@ -45,6 +51,12 @@ void handle_audio(void) {
       // Write to the hardware FIFOs
       audiop->left_fifo = left;
       audiop->right_fifo = right;
+
+      //convolve
+      int result[2] = 0;
+      convolve(audio_word_array, &result);
+      
+
 
       // Advance the index. (Change to += 11 if you still need to speed it up)
       left_index_counter++;
@@ -60,4 +72,21 @@ void handle_audio(void) {
     }
   }
   return 0;
+}
+
+const short** fourty_five_deg_hrtf_left = (const short*) hrtf_left_matrix;
+const short** fourty_five_deg_hrtf_right = (const short*) hrtf_right_matrix;
+
+int* convolve(const short* audio_word_array, int* result){
+  if(left_index_counter < HRTF_LENGTH){
+    for(int i = 0; i <= left_index_counter; i++){
+      result[0] += (audio_word_array[left_index_counter - i] * hrtf_left_matrix[10][i]);
+      result[1] += (audio_word_array[right_index_counter - i] * hrtf_right_matrix[10][i]);
+    }
+  }else{
+    for(int i = 0; i <= (HRTF_LENGTH - 1); i++){
+      result[0] += (audio_word_array[HRTF_LENGTH - i + left_index_counter] * hrtf_left_matrix[10][i]);
+      result[1] += (audio_word_array[HRTF_LENGTH - i + left_index_counter] * hrtf_right_matrix[10][i]);
+    }
+  }
 }
